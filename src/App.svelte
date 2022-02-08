05bc998cd4fc6c76b6207b8ab16a7eb43a76a132
Ricard Torres <torres.rick@gmail.com>
2022-02-08 12:31:10 +0100

    2.0.0
    Add Academy

--- src/App.svelte
+++ src/App.svelte
@@ -4,12 +4,28 @@
 	let items = [];
 	let candidateName = '';
 	const output = [];
+	let data = {};
+	let grades = [];
+	let currentGrade = 0;
 
 	onMount(async function () {
 		const response = await fetch('data.json');
-		const data = await response.json();
-		items = data.evaluationItems;
+		data = await response.json();
+		items = data.associate;
+		grades = data.grades;
 	});
+
+	function onGradeChange(event) {
+		items = event.target.checked ? data.programmer : data.associate
+	}
+
+	function copyToClipboard() {
+		const text =
+`${output.filter(i => i).join('\n\n').replaceAll('{CANDIDATE}', candidateName)}\n
+Front-end Software Engineer Level Technical Recommendation: ${grades.filter(i => i.id === currentGrade)[0].name }`;
+
+		navigator.clipboard.writeText(text);
+	}
 </script>
 
 <header>
@@ -22,6 +38,20 @@
 		<h2>Basic Information</h2>
 		<label for="candidate-name">Candidate Name</label>
 		<input type="text" id="candidate-name" name="candidate-name" bind:value={candidateName} required>
+
+		{#if candidateName !== ''}
+		<div class="form-check form-switch">
+			<input
+				class="form-check-input"
+				type="checkbox"
+				role="switch"
+				id="academy"
+				value="true"
+				on:change={onGradeChange}>
+				<label class="form-check-label" for="academy">Academy interview</label>
+		</div>
+		{/if}
+
 		{#if candidateName !== ''}
 			<ul>
 				{#each items as item}
@@ -43,9 +73,24 @@
 				{/each}
 			</ul>
 		{/if}
+
+		{#if grades.length > 0 && candidateName !== ''}
+		<label for="customRange3" class="form-label">Grade ({ grades.filter(i => i.id === currentGrade)[0].name })</label>
+		<input type="range" class="form-range" min=0 max={ grades.length - 1 } bind:value={currentGrade} step=1 id="customRange3">
+		{/if}
 	</form>
-	<h2>Output</h2>
-	<div class="output">{@html output.filter(i => i).join('<br /><br />').replaceAll('{CANDIDATE}', candidateName)}</div>
+	{#if candidateName !== ''}
+		<h2>Output</h2>
+		<div class="output">
+			{@html output.filter(i => i).join('<br /><br />').replaceAll('{CANDIDATE}', candidateName)}
+
+			{#if grades.length > 0 }
+			<br /><br />
+			<strong>Front-end Software Engineer Level Technical Recommendation</strong>: { grades.filter(i => i.id === currentGrade)[0].name }
+			{/if}
+		</div>
+		<button type="button" class="btn btn-primary" on:click={copyToClipboard}>Copy to clipboard</button>
+	{/if}
 </main>
 <footer>
 	<div class="container">
